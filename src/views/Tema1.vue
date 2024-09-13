ce13a1479b72cdb841e5270706dd3aded38994ef
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2024-09-13 14:16:14 -0500

    avance

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -2,35 +2,57 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 1
+      h1 Prevención y minimización
 
-    //- El componente "Muestras" tiene las aplicaciones de todos los componentes de la base
-    //- se encuentra en src/components/Muestras.vue
-    //- Una vez el componente "Muestras" no se necesite 
-    //- Se debe borrar el "import Muestras from '../components/Muestras'" y en "components" en "<script"
-    //- esto evitará que se compile en la carpeta final de distribución
-    Muestras
-  
+    #t_1_1.titulo-segundo.color-acento-contenido(data-aos="zoom-in-left")
+      h2 1.1¿Qué es un residuo peligroso?
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="zoom-in-left")
+      .col-lg-1
+        img(src='@/assets/curso/temas/tema1/img01.svg', alt='')
+      .col-lg-11 
+        p Según la Universidad Pedagógica Nacional un residuo peligroso es aquel que por sus características intrínsecas puede causar daño a la salud humana o al ambiente. Algunas características que otorgan a un residuo el rango de peligroso son:
+
+    .row.justify-content-center.align-items-center.mb-2
+      .col-lg-5
+        .titulo-sexto.color-acento-contenido
+          h5 Figura 1.
+          span
+            | Características de los residuos peligrosos
+
+    .row.justify-content-center.align-items-center(data-aos="zoom-in-left")
+      .col-lg-12
+        ImagenInfografica.color-primario
+          template(v-slot:imagen)
+            figure
+              img(src='@/assets/curso/temas/tema1/infografia.svg', alt='en el diagrama se enuncian las siguientes características: tóxico, corrosivo, infeccioso, explosivo, inflamable, ecotóxico, reactivo.')
+    
+          .tarjeta.color-primario.p-3(x="52%" y="27%" numero="+")
+            .h5.mb-2.text-center Explosivo
+
+          .tarjeta.color-primario.p-3(x="37%" y="43%" numero="+")
+            .h5.mb-2.text-center Corrosivo
+
+          .tarjeta.color-primario.p-3(x="34%" y="73%" numero="+")
+            .h5.mb-2.text-center Ecotóxico
+
+          .tarjeta.color-primario.p-3(x="44%" y="95%" numero="+")
+            .h5.mb-2.text-center Reactivo   
+
+          .tarjeta.color-primario.p-3(x="59%" y="95%" numero="+")
+            .h5.mb-2.text-center Inflamable     
+
+          .tarjeta.color-primario.p-3(x="69%" y="73%" numero="+")
+            .h5.mb-2.text-center Tóxico  
+
+          .tarjeta.color-primario.p-3(x="67%" y="43%" numero="+")
+            .h5.mb-2.text-center Infeccioso     
+            
+      separador
+      #t_1_2.titulo-segundo.color-acento-contenido(data-aos="zoom-in-left")
+        h2 1.2 ¿Qué es un plan de gestión integral de residuos peligrosos? 
+       
 </template>
-
-<script>
-import Muestras from '../components/Muestras' // borrar una vez el componente "Muestras" no se necesite
-export default {
-  name: 'Tema1',
-  components: {
-    Muestras, // borrar una vez el componente "Muestras" no se necesite
-  },
-  data: () => ({
-    // variables de vue
-  }),
-  mounted() {
-    this.$nextTick(() => {
-      this.$aosRefresh()
-    })
-  },
-  updated() {
-    this.$aosRefresh()
-  },
-}
-</script>
-
-<style lang="sass"></style>
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,15 +3,18 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    p Con el diagrama de actividades se termina la descripción de las tres técnicas usadas para la especificación y el análisis de requisitos, como se pudo analizar cada una de las técnicas nos brinda características para lograr representar gráficamente y de una forma clara, los requisitos que un usuario necesita poder cumplir satisfactoriamente con las solicitudes de los clientes y usuarios, por otro lado se pueden usar todas las técnicas en una especificación y análisis de requisitos si se ve la necesidad.
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
+        img(src="@/assets/curso/titulo.svg", alt="alt")
+        .py-3
+        p.mb-5 Identificar correctamente las fuentes de generación de residuos peligrosos y plantearse objetivos y metas para su minimización le permite reducir costos a las organizaciones, y contribuye a la preservación del medio ambiente. Por su parte, el buen manejo interno de los residuos peligrosos permite mantener seguros a los empleados, visitantes y clientes. En el siguiente cuadro, se presentan los conceptos claves trabajados dentro de este componente formativo:
 
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
           img(src="@/assets/curso/sintesis.svg", alt="alt")
       .col-auto
-        a.anexo.mb-4(:href="obtenerLink('/downloads/prueba.pdf')" target="_blank")
+        a.anexo.mb-4(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
             img(src="@/assets/template/icono-pdf.svg")
           .anexo__texto
