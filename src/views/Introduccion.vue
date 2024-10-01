6cf0e80c72e87ca734176c81a23dc74fa1d76ecd
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2024-10-01 14:27:05 -0500

    avance

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,8 +13,8 @@
     
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/sVXc6R_kd40" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video: Prevención, minimización y manejo interno residuos sólidos peligrosos
     
    
 </template>
